c614a327cd09b1b0b3dfba81570a51a35a8ce330
gga01075 <gga01075@naver.com>
2022-12-29 14:19:34 +0900

    cart item setting

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 
-import Modal from '../UI/Modal'
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0 ? true : false;
 
-const Cart = props => {
-
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
 
   const cartItmes = (
     <ul className={classes[`cart-items`]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li key={item.id}>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -20,13 +33,14 @@ const Cart = props => {
       {cartItmes}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes[`button--alt`]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {/* Order 버튼은 항목이 있을때만 나타나게 */}
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
